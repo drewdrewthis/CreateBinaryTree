160ab32d430fba9bde406e40cdf9841e2fe606d1
Andrew Joia <drewdrewthis@gmail.com>
2016-06-24 18:37:56 -0400

    Add js

---
+++ BST.js
@@ -0,0 +1,85 @@
+var min = 0;
+var max = 100;
+var treeSize = 4;
+
+function getRandomIntInclusive(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomNumber() {
+  return getRandomIntInclusive(min, max);
+}
+
+function BinaryTreeNode(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+}
+
+BinaryTreeNode.prototype.insertLeft = function(value) {
+  this.left = new BinaryTreeNode(value);
+  return this.left;
+};
+
+BinaryTreeNode.prototype.insertRight = function(value) {
+  this.right = new BinaryTreeNode(value);
+  return this.right;
+};
+
+var newTree;
+
+function makeLeftBranches(tree, h) {
+
+  h--;
+  console.log("L" + h);
+  var value = getRandomNumber();
+
+  if (h > 0) {
+    tree.insertLeft(value);
+    makeRightBranches(tree.left, h);
+    makeLeftBranches(tree.left, h);
+    return;
+  } else return;
+}
+
+function makeRightBranches(tree, h) {
+
+  h--;
+  console.log("R" + h);
+  var value = getRandomNumber();
+
+  if (h > 0) {
+    tree.insertRight(value);
+    makeRightBranches(tree.right, h);
+    makeLeftBranches(tree.right, h);
+    return;
+  } else return;
+}
+
+
+function buildTree(h) {
+
+  var value = getRandomNumber();
+  newTree = new BinaryTreeNode(value);
+
+  makeLeftBranches(newTree, h);
+  makeRightBranches(newTree, h);
+
+  return newTree;
+}
+
+var tree = new buildTree(treeSize); // Will build a tree 4 levels deep
+
+console.log(JSON.stringify(tree));
+
+
+function isBSTValid(bst) {
+  // write the body of your function here
+
+  buildTree(4);
+  return true;
+}
+
+// run your function through some test cases here
+// remember: debugging is half the battle!
+console.log(isBSTValid('test input'));
